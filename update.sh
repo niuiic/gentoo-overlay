cb98822c83116aaa99b7e83ff8f7076dda388be9
niuiic <niuiicde@gmail.com>
2022-09-07 13:52:06 +0800

    remove glsl-language-server

--- update.sh
+++ update.sh
@@ -12,4 +12,3 @@ check "https://github.com/artemsen/swayimg/releases" swayimg
 check "https://github.com/rofl0r/proxychains-ng/releases" proxychains
 check "https://github.com/chipsalliance/verible/releases" verible-bin
 check "https://github.com/keshavbhatt/ColorPicker/releases" color-picker
-check "https://github.com/billyb2/glsl-language-server-gentoo/releases" glsl-language-server
