0517a2b385dcb6c717b29ca66c1b26439a1943b6
niuiic <niuiicde@gmail.com>
2024-06-12 20:32:03 +0800

    update

--- update.sh
+++ update.sh
@@ -11,4 +11,3 @@ echo -e "check https://devmanual.gentoo.org/quickstart/index.html for tips to wr
 check "https://github.com/rofl0r/proxychains-ng/releases" proxychains
 check "https://github.com/patriciogonzalezvivo/glslViewer/releases" glslviewer
 check "https://docs.flutter.dev/get-started/install/linux/android?tab=download#download-then-install-flutter" flutter
-check "https://github.com/containers/podman-desktop/releases?q=v1&expanded=true" podman-desktop
